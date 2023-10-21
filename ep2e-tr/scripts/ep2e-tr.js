0357442f6a932b20f84f99b9e9e0b592e9bf61cd
Erik Itter <erik.itter@gmail.com>
2023-10-21 06:12:45 +0200

    module init

--- ep2e-tr/scripts/ep2e-tr.js
+++ ep2e-tr/scripts/ep2e-tr.js
@@ -1,3 +1,207 @@
 var start_time = performance.now();
-console.log("Tablerules is loading.");
+console.log("tablerules is loading.");
+
+const MODULE_SCOPE = "tablerules";
+
+class TRUtils {
+
+    static isDebugEnabled() {
+        return (game.settings.get(MODULE_SCOPE, "logLevel") >= 3);
+    }
+
+    static registerSettings() {
+        game.settings.register(MODULE_SCOPE, 'isEnabled', {
+            name: "Enable Tablerules",
+            hint: "Enables Tablerules Module changes. If we ever implement this disabling this setting will make all other Tablerules settings be ignored and return the stuff that has settings configured return to what it is without the Module. This has no effect as of now, and might just get removed instead of being implemented in the future.",
+            scope: 'world',
+            config: true,
+            default: true,
+            type: Boolean,
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, "whispersIncludeGM", {
+            name: "Whispers, add GM",
+            hint: "adds the GM to all whispered chat messages",
+            scope: "world",
+            config: true,
+            default: true,
+            type: Boolean,
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, "modifyDefaultVolumes", {
+            name: "Modify Default Volumes",
+            hint: "modified the core default volumes, if enabled the Module checks the current settings and adjusts them to the configured below values if they are at assumed core default values.",
+            scope: "world",
+            config: true,
+            default: true,
+            type: Boolean,
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, "globalPlaylistVolume", {
+            name: "globalPlaylistVolume default overwrite value",
+            hint: "if Modify Default Volumes is enabled this overwrites the core default",
+            scope: "world",
+            config: true,
+            default: 0.1,
+            type: Number,
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, "globalAmbientVolume", {
+            name: "globalAmbientVolume default overwrite value",
+            hint: "if Modify Default Volumes is enabled this overwrites the core default",
+            scope: "world",
+            config: true,
+            default: 0.1,
+            type: Number,
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, "globalInterfaceVolume", {
+            name: "globalInterfaceVolume default overwrite value",
+            hint: "if Modify Default Volumes is enabled this overwrites the core default",
+            scope: "world",
+            config: true,
+            default: 0.1,
+            type: Number,
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, "logLevel", {
+            name: "Log Level",
+            hint: "The Module's own log level. By default FVTT and the module don't log debug and info. Set to error for normal operation and debug for development.",
+            scope: 'world',
+            config: true,
+            default: "error",
+            type: Number,
+            choices: {
+                0: "error",
+                1: "warning",
+                2: "info",
+                3: "debug"
+            },
+            requiresReload: true
+        });
+
+        game.settings.register(MODULE_SCOPE, 'logOwn', {
+            name: "Use own logging function.",
+            hint: "Enable to log using own logging method. Disable for play and enable for development if debugging (with Log Level set to debug above).",
+            scope: 'world',
+            config: true,
+            default: false,
+            type: Boolean,
+            requiresReload: true
+        });
+    }
+}
+
+class Tablerules {
+    static log(level, message) {
+        let levelstring;
+
+        switch (level) {
+            case 0:
+                levelstring = "ERROR";
+                break;
+            case 1:
+                levelstring = "WARNING";
+                break;
+            case 2:
+                levelstring = "INFO";
+                break;
+            case 3:
+                levelstring = "DEBUG";
+                break;
+            default:
+                console.error("No logging level set.");
+                console.error(message);
+        }
+
+        if (typeof message === "object") {
+            console.log(message);
+        } else {
+            console.log({ message: "Tablerules | " + levelstring + ":" + message, obj: typeof message === "object" ? message : null });
+        }
+    }
+
+    static debug(message) {
+        if (Tablerules.config.loglevel < 3)
+            return;
+        if (Tablerules.config.logOwn) {
+            Tablerules.log(3, message);
+        } else {
+            console.debug(message);
+        }
+    }
+
+    static error(message) {
+        if (Tablerules.config.logOwn) {
+            Tablerules.log(0, message);
+        } else {
+            console.error(message);
+        }
+    }
+
+    static warn(message) {
+        if (Tablerules.config.loglevel < 1)
+            return;
+        if (Tablerules.config.logOwn) {
+            Tablerules.log(1, message);
+        } else {
+            console.warn(message);
+        }
+    }
+
+    static info(message) {
+        if (Tablerules.config.loglevel < 2)
+            return;
+        if (Tablerules.config.logOwn) {
+            Tablerules.log(2, message);
+        } else {
+            console.info(message);
+        }
+    }
+}
+
+Hooks.on('init', () => {
+    TRUtils.registerSettings();
+});
+
+
+Hooks.on("preCreateChatMessage", (messageDoc, rawMessageData, context, userId) => {
+
+    if (!game.settings.get(MODULE_SCOPE, "whispersIncludeGM") || !game.settings.get(MODULE_SCOPE, "isEnabled")) {
+        return;
+    }
+
+    const gmWhisperIds = ChatMessage.getWhisperRecipients("gm").map(i => i.id) // get all gm ids in the world
+    let whisperArray = duplicate(messageDoc.whisper) // Copy our array out
+    if (whisperArray.length === 0) return // Not a whisper if there's no whisper ids
+
+
+    for (let gmId of gmWhisperIds) {// Push each gm id into the array of whisper ids
+        if (gmId === game.user.id) continue // You never include yourself in the whisper so this would erronously add yourself causing the "we changed the array! trigger later on"
+        if (!whisperArray.includes(gmId)) {
+            whisperArray.push(gmId)
+        }
+    }
+
+    if (whisperArray.length !== messageDoc.whisper.length) { //only modify if needed
+        let userListString = ""
+        for (let userId of messageDoc.whisper) {
+            userListString = userListString + game.users.get(userId).name + ", "
+        }
+        userListString = userListString.slice(0, -2)
+
+        messageDoc.updateSource({
+            content: `${messageDoc.content}`,//<br>Original Whisper Recipients: ${userListString}`,
+            whisper: whisperArray
+        })
+    }
+});
+
+
 console.log(`Tablerules has been loaded (${performance.now() - start_time}ms).`);
