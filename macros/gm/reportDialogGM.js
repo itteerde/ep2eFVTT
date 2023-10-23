107e8c835c5c1084a2d651f30b8dd48e7eca1357
Erik Itter <erik.itter@gmail.com>
2023-10-23 12:34:37 +0200

    Issue #14

--- macros/gm/reportDialogGM.js
+++ macros/gm/reportDialogGM.js
@@ -2,6 +2,8 @@
 const partyIds = [
     "FTcEjiRQ0DU6Wwpe", // EGR_2.71828
     "GWpcf0ilVIRkl17D", // Kumo Noire
+    "I5zQ7SgKvicAwf1A", // Nilk Narf
+    "W2s7B6H7vlv87OiX", // Sith
     "KetJuJd3Ufj4klCF" // SysRig.ExE
 ];
 
@@ -11,16 +13,9 @@ const width = 1000;
 const party = partyIds.map(i => game.actors.get(i));
 //console.log(party);
 
-/**
-         ${party.reduce(function(html,actor){
-            return html+"<div class='chat-message message' style='display: flex; flex-direction: row; justify-content: space-evenly; align-items:left; align-content:center;'>"
-        })}
-
- */
-
 let dialogContent = `<div width=${width}px><h1>Summary</h1>`;
-dialogContent += party.reduce(function(html,actor){
-    return html+`
+dialogContent += party.reduce(function (html, actor) {
+    return html + `
         <div class="chat-message message" style="display: flex; flex-direction: row; justify-content: space-evenly; align-items:left; align-content:center;">
             <div>${actor.name}</div>
             <div>
@@ -43,27 +38,8 @@ dialogContent += party.reduce(function(html,actor){
             </div>
         </div>
     `;
-},``);
+}, ``);
 dialogContent += "</div>"
-
-
-/**
- let dialogContent = `
-    <div width=${width}px>
-        <h1>Summary</h1>
-        <div class="chat-message message" style="display: flex; flex-direction: row; justify-content: space-evenly; align-items:left; align-content:center;">
-            <div>EGR_2.71828</div><div><i class="fa-solid fa-heart-pulse awesomeIcon" style="font-size: 18px;"></i>0/40/80</div><div><i class="fa-regular fa-bandage awesomeIcon" style="font-size: 18px;"></i>0 (8)</div><div><i class="fa-solid fa-brain awesomeIcon" style="font-size: 18px;"></i>0/30/60</div><div><i class="fa-regular fa-burst awesomeIcon" style="font-size: 18px;"></i>0 (6)</div><div>AE 0</div><div>AK 0</div>
-        </div>
-        <div class="chat-message message" style="display: flex; flex-direction: row; justify-content: space-evenly; align-items:left; align-content:center;">
-            <div>EGR_2.71828</div><div><i class="fa-solid fa-heart-pulse awesomeIcon" style="font-size: 18px;"></i>0/40/80</div><div><i class="fa-regular fa-bandage awesomeIcon" style="font-size: 18px;"></i>0 (8)</div><div><i class="fa-solid fa-brain awesomeIcon" style="font-size: 18px;"></i>0/30/60</div><div><i class="fa-regular fa-burst awesomeIcon" style="font-size: 18px;"></i>0 (6)</div><div>AE 0</div><div>AK 0</div>
-        </div>
-        <div class="chat-message message" style="display: flex; flex-direction: row; justify-content: space-evenly; align-items:left; align-content:center;">
-            <div>EGR_2.71828</div><div><i class="fa-solid fa-heart-pulse awesomeIcon" style="font-size: 18px;"></i>0/40/80</div><div><i class="fa-regular fa-bandage awesomeIcon" style="font-size: 18px;"></i>0 (8)</div><div><i class="fa-solid fa-brain awesomeIcon" style="font-size: 18px;"></i>0/30/60</div><div><i class="fa-regular fa-burst awesomeIcon" style="font-size: 18px;"></i>0 (6)</div><div>AE 0</div><div>AK 0</div>
-        </div>
-    </div>`;
-
-
- */
 
 let d = new Dialog({
     title: macroLabel,
@@ -74,9 +50,9 @@ let d = new Dialog({
             label: "",
         },
     },
-    default: "one",
-    render: html => console.log("Register interactivity in the rendered dialog"),
-    close: html => console.log("This always is logged no matter which option is chosen")
+    default: "one"//,
+    //render: html => console.log("Register interactivity in the rendered dialog"),
+    //close: html => console.log("This always is logged no matter which option is chosen")
 });
 d.render(true, { width: width });
 
