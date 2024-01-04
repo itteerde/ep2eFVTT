b9428043b7c6e1e2faf6e580ff5383d5898bd377
Erik Itter <erik.itter@gmail.com>
2024-01-04 05:38:49 +0100

    ...

--- macros/player/reportSkill.js
+++ macros/player/reportSkill.js
@@ -14,8 +14,9 @@ const buttons = {
         label: "Report",
         callback: async (html) => {
             const skillName = html[0].querySelector("#skillName").value;
+            const skill = TREP2eDB.skills.find(s => s.name === skillName);
 
-            await ChatMessage.create({ content: "<h3>" + skillName + "</h3><table>" + TRTacNet.shortenReportSkill(TRTacNet.reportSkill(skillName)).reduce((acc, e) => { return acc += `<tr><td>${e.name}</td><td>${e.roll}</td></tr>`; }, "") + "</table>" });
+            await ChatMessage.create({ content: "<h3>" + skillName + "</h3><table>" + TRTacNet.shortenReportSkill(TRTacNet.reportSkill(skillName)).reduce((acc, e) => { return acc += `<tr><td>${e.name}</td><td>${e.roll}</td></tr>`; }, "") + "</table>" + `<div>Aptitude: ${skill.aptitude}</div><div>Types: ${skill.types.reduce((acc, t) => { return acc += t + ", " }, "")}</div>` });
             return;
         }
     }
